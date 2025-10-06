bdde690f00c18c64de2b1b46b9e817bd925b9716
ritvik <ritvikchandrikapure@gmail.com>
2025-10-06 16:40:42 +0530

    Add payment.css

--- frontend/src/Pages/Payment.jsx
+++ frontend/src/Pages/Payment.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
-import "./payment.css";
+import "./Payment.css";
 import Footer from '../component/Footer';
 import Navigation from '../component/Navigation';
 
